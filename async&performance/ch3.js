90f159b5bdf96a1b69cf2e455b6f2cb08786077b
BenceHordos523 <Bence.Hordos523@gmail.com>
2019-03-14 14:16:56 +0100

    Promise - until Error handling, huge and nice topic tho

---
+++ async&performance/ch3.js
@@ -0,0 +1,111 @@
+// PROMISES
+
+// the first promise example
+function add(xPromise,yPromise) {
+	// `Promise.all([ .. ])` takes an array of promises,
+	// and returns a new promise that waits on them
+	// all to finish
+	return Promise.all( [xPromise, yPromise] )
+
+	// when that promise is resolved, let's take the
+	// received `X` and `Y` values and add them together.
+	.then( function(values){
+		// `values` is an array of the messages from the
+		// previously resolved promises
+		return values[0] + values[1];
+	} );
+}
+
+// `fetchX()` and `fetchY()` return promises for
+// their respective values, which may be ready
+// *now* or *later*.
+add( fetchX(), fetchY() )
+
+// we get a promise back for the sum of those
+// two numbers.
+// now we chain-call `then(..)` to wait for the
+// resolution of that returned promise.
+.then( function(sum){
+	console.log( sum ); // that was easier!
+} );
+
+// handle error:
+add( fetchX(), fetchY() )
+.then(
+	// fulfillment handler
+	function(sum) {
+		console.log( sum );
+	},
+	// rejection handler
+	function(err) {
+		console.error( err ); // bummer!
+	}
+);
+
+
+// baz and bar subscribing to the promise
+function foo(x) {
+	// start doing something that could take a while
+
+	// construct and return a promise
+	return new Promise( function(resolve,reject){
+		// eventually, call `resolve(..)` or `reject(..)`,
+		// which are the resolution callbacks for
+		// the promise.
+	} );
+}
+
+var p = foo( 42 );
+
+bar( p );
+
+baz( p );
+
+// bar doing this
+function bar(fooPromise) {
+	// listen for `foo(..)` to complete
+	fooPromise.then(
+		function(){
+			// `foo(..)` has now finished, so
+			// do `bar(..)`'s task
+		},
+		function(){
+			// oops, something went wrong in `foo(..)`
+		}
+	);
+}
+
+// if there is a problem, the promise will always be rejected
+var p = new Promise( function(resolve,reject){
+	foo.bar();	// `foo` is not defined, so error!
+	resolve( 42 );	// never gets here :(
+} );
+
+p.then(
+	function fulfilled(){
+		// never gets here :(
+	},
+	function rejected(err){
+		// `err` will be a `TypeError` exception object
+		// from the `foo.bar()` line.
+	}
+);
+
+// assume an `ajax( {url}, {callback} )` utility
+
+// Promise-aware ajax
+function request(url) {
+	return new Promise( function(resolve,reject){
+		// the `ajax(..)` callback should be our
+		// promise's `resolve(..)` function
+		ajax( url, resolve );
+	} );
+}
+
+request( "http://some.url.1/" )
+.then( function(response1){
+	return request( "http://some.url.2/?v=" + response1 );
+} )
+.then( function(response2){
+	console.log( response2 );
+} );
