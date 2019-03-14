18ddb6ac66b44d7f3eb5e2af65deade44b7d2851
BenceHordos523 <Bence.Hordos523@gmail.com>
2019-03-14 17:47:54 +0100

    Promises, the END, huge!

--- async&performance/ch3.js
+++ async&performance/ch3.js
@@ -109,3 +109,38 @@ request( "http://some.url.1/" )
 .then( function(response2){
 	console.log( response2 );
 } );
+
+
+// back - Error handling
+
+// Chaining
+// Promise akk
+var p1 = request( "http://some.url.1/" );
+var p2 = request( "http://some.url.2/" );
+
+Promise.all( [p1,p2] )
+.then( function(msgs){
+	// both `p1` and `p2` fulfill and pass in
+	// their messages here
+	return request(
+		"http://some.url.3/?v=" + msgs.join(",")
+	);
+} )
+.then( function(msg){
+	console.log( msg );
+} );
+
+var p2 = request( "http://some.url.2/" );
+
+
+// Promise race
+Promise.race( [p1,p2] )
+.then( function(msg){
+	// either `p1` or `p2` will win the race
+	return request(
+		"http://some.url.3/?v=" + msg
+	);
+} )
+.then( function(msg){
+	console.log( msg );
+} );
